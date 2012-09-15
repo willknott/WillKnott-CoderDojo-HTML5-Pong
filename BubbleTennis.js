02b69ebdba23144fe76fe6fb5666cbf7e7c54733
Will K <Will@Willknott.ie>
2012-09-15 09:42:21 +0100

    Second class' merge
    Signed-off-by: Will K <Will@Willknott.ie>

--- BubbleTennis.js
+++ BubbleTennis.js
@@ -1,9 +1,9 @@
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 612;
+canvas.width = 212;
 canvas.width = document.body.clientWidth;
-canvas.height = 480;
+canvas.height = 280;
 canvas.height = document.body.clientHeight;
 //    http://github.com/willknott
 document.body.appendChild(canvas);
@@ -11,22 +11,18 @@ document.body.appendChild(canvas);
 
 var grd = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
 grd.addColorStop(0, '#ff0000');
+// R G B A
 grd.addColorStop(1, '#ffffff');
 ctx.fillStyle = grd;
 
-
-// Background
 var bgReady = false;
 window.onload = function () {
 //different way to create a function
 bgReady = true;
 };
 
-
-
 // Draw everything
 var render = function () {
-ctx.fillStyle = 'rgba(00, 00, 55, 0.6)';
 ctx.fillStyle = grd;
 ctx.fillRect (0, 0, canvas.width, canvas.height);
 }
@@ -35,5 +31,8 @@ ctx.fillRect (0, 0, canvas.width, canvas.height);
 var main = function () {
 render();
 };
-// Let's play this game!
+
+// Let's start this game!
 setInterval(main, 1); // Execute as fast as possible
+
+//use google / bing / yahoo / etc to find out about "Request Animation Javascript"
